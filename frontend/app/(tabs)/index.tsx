0faeb3dad450248d7010f3e9739f05a94eb33a62
Johan Juarez <juarezjohan650@gmail.com>
2025-04-26 15:39:01 -0700

    finished version
    of the frontend

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -50,7 +50,7 @@ const App = () => {
   const [currentChatId, setCurrentChatId] = useState<string | null>(null);
   const [isSidebarVisible, setIsSidebarVisible] = useState(false);
 
-  const BACKEND_URL = 'http://IPDRESS CHANGE THIS FIRST GODDAMN IT:3001';
+  const BACKEND_URL = 'http://192.168.1.78:3001';
   const flatListRef = useRef<FlatList>(null);
   const insets = useSafeAreaInsets();
 
